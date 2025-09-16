e584e753c12318de1a48bc3e22aea924cc9dbab4
Denny  Arfansyah <denny.arfansyah@silentmode.my>
2025-09-16 08:34:58 +0700

    Create the VideoItem model

---
+++ LearningDeutsch/VideoItem.swift
@@ -0,0 +1,16 @@
+//
+//  VideoItem.swift
+//  LearningDeutsch
+//
+//  Created by Denny Arfansyah on 16/09/2025.
+//
+
+import Foundation
+
+struct VideoItem {
+    let id: UUID
+    let title: String
+    let description: String?
+    let thumbnailURL: URL
+    let videoURL: URL
+}
