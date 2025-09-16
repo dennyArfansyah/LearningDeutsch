af2439d468b7acc82c5f123291e33d59a24254e3
Denny  Arfansyah <denny.arfansyah@silentmode.my>
2025-09-16 11:46:26 +0700

    Refactor test to remove duplication

--- LearningDeutschTests/RemoteVideoLoaderTests.swift
+++ LearningDeutschTests/RemoteVideoLoaderTests.swift
@@ -25,31 +25,37 @@ protocol HTTPClient {
     func get(from url: URL)
 }
 
-class HTTPClientSpy: HTTPClient {
-    var requestURL: URL?
-    
-    func get(from url: URL) {
-        requestURL = url
-    }
-}
-
 class RemoteVideoLoaderTests: XCTestCase {
     
     func test_init_doestNotRequestDataFromURL() {
-        let url = URL(string: "http://a-given-url.com")!
-        let client = HTTPClientSpy()
-        _ = RemoteVideoLoader(url: url, client: client)
+        let (_, client) = makeSUT()
         
         XCTAssertNil(client.requestURL)
     }
     
     func test_load_requestDataFromURL() {
         let url = URL(string: "http://a-given-url.com")!
-        let client = HTTPClientSpy()
-        let sut = RemoteVideoLoader(url: url, client: client)
+        let (sut, client) = makeSUT(url: url)
         
         sut.load()
         
         XCTAssertEqual(client.requestURL, url)
+    }
+    
+    //MARK: - Helpers
+    
+    private func makeSUT(url: URL = URL(string: "http://a-url.com")!) -> (sut: RemoteVideoLoader, client: HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let sut = RemoteVideoLoader(url: url, client: client)
+        
+        return (sut, client)
+    }
+    
+    private class HTTPClientSpy: HTTPClient {
+        var requestURL: URL?
+        
+        func get(from url: URL) {
+            requestURL = url
+        }
     }
 }
