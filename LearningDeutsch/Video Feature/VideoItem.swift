5a402c157a98c97450b9d919cb18e2d030905d21
Denny  Arfansyah <denny.arfansyah@silentmode.my>
2025-09-17 23:06:22 +0700

    Create factory method to simplify setup

--- LearningDeutsch/Video Feature/VideoItem.swift
+++ LearningDeutsch/Video Feature/VideoItem.swift
@@ -8,9 +8,24 @@
 import Foundation
 
 public struct VideoItem: Equatable {
-    let id: UUID
-    let title: String
-    let description: String?
-    let thumbnailURL: URL
-    let videoURL: URL
+    public let id: UUID
+    public let description: String?
+    public let location: String?
+    public let imageURL: URL
+    
+    public init(id: UUID, description: String?, location: String?, imageURL: URL) {
+        self.id = id
+        self.description = description
+        self.location = location
+        self.imageURL = imageURL
+    }
+}
+
+extension VideoItem: Decodable {
+    private enum CodingKeys: String, CodingKey {
+        case id
+        case description
+        case location
+        case imageURL = "image"
+    }
 }
--- LearningDeutschTests/RemoteVideoLoaderTests.swift
+++ LearningDeutschTests/RemoteVideoLoaderTests.swift
@@ -74,6 +74,24 @@ class RemoteVideoLoaderTests: XCTestCase {
         })
     }
     
+    func test_load_deliveersItemsOn200HTTPResponseWithValidJSONItems() {
+        let (sut, client) = makeSUT()
+        
+        let item1 = makeItem(id: UUID(), imageURL: URL(string: "https://a-url.com")!)
+        let item2 = makeItem(id: UUID(),
+                              description: "a description",
+                              location: "a location",
+                              imageURL: URL(string: "https://another-url.com")!)
+        
+        
+        let items = [item1.model, item2.model]
+        
+        expect(sut, toCompleteWithResult: .success(items), when: {
+            let json = makeItemsJSON([item1.json, item2.json])
+            client.complete(withStatusCode: 200, data: json)
+        })
+    }
+    
     //MARK: - Helpers
     
     private func makeSUT(url: URL = URL(string: "http://a-url.com")!) -> (sut: RemoteVideoLoader, client: HTTPClientSpy) {
@@ -81,6 +99,25 @@ class RemoteVideoLoaderTests: XCTestCase {
         let sut = RemoteVideoLoader(url: url, client: client)
         
         return (sut, client)
+    }
+    
+    private func makeItem(id: UUID, description: String? = nil, location: String? = nil, imageURL: URL) -> (model: VideoItem, json: [String: Any]) {
+        let item = VideoItem(id: id, description: description, location: location, imageURL: imageURL)
+        let json = [
+            "id": id.uuidString,
+            "description": description,
+            "location": location,
+            "image": imageURL.absoluteString
+        ].reduce(into: [String: Any]()) { (acc, e) in
+            if let value = e.value { acc[e.key] = value }
+        }
+        
+        return (item, json)
+    }
+    
+    private func makeItemsJSON(_ items: [[String: Any]]) -> Data {
+        let json = ["items": items]
+        return try! JSONSerialization.data(withJSONObject: json)
     }
     
     private func expect(_ sut: RemoteVideoLoader,
