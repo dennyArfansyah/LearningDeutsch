b946a1bf9c41dcea32e7baedf7d08cacf8e853c2
Denny  Arfansyah <denny.arfansyah@silentmode.my>
2025-09-18 22:53:18 +0700

    Move VideoItemsMAppers to its own file

---
+++ LearningDeutsch/Video API/VidemItemsMapper.swift
@@ -0,0 +1,40 @@
+//
+//  VidemItemsMapper.swift
+//  LearningDeutsch
+//
+//  Created by Denny Arfansyah on 18/09/2025.
+//
+
+import Foundation
+
+final class VidemItemsMapper {
+    private struct Root: Decodable {
+        let items: [Item]
+    }
+
+    private struct Item: Decodable {
+        let id: UUID
+        let description: String?
+        let location: String?
+        let image: URL
+        
+        var item: VideoItem {
+            return VideoItem(id: id,
+                             description: description,
+                             location: location,
+                             imageURL: image)
+        }
+    }
+    
+    
+    private static var OK_200: Int { 200 }
+    
+    static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [VideoItem] {
+        guard response.statusCode == OK_200 else {
+            throw RemoteVideoLoader.Error.invalidData
+        }
+        
+        let root = try JSONDecoder().decode(Root.self, from: data)
+        return root.items.map { $0.item }
+    }
+}
